aca95e7f7a7f6d66d94ac7a3789c14f88c629ece
Berkay <sberkay.ergin@tedu.edu.tr>
2018-11-28 00:46:49 +0200

    Bullet class has been added & WeaponBuilder finalized

--- HeavenGuard/src/elements/BaseWeapon.java
+++ HeavenGuard/src/elements/BaseWeapon.java
@@ -13,25 +13,27 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-import com.sun.media.sound.ModelSource;
-
-import elements.WeaponFactory.Builder;
 
 public class BaseWeapon extends ImageIcon {
 
 	private int fireSpeed, damage, weaponLevel,  reloadTime;
+	
+	// Canonical paths of image files
 
 	static final String CANNON_PATH = "HeavenGuard/res/images/cannongun/weapon_cannon_0.png";
 	static final String MACHINEGUN_PATH = "HeavenGuard/res/images/machinegun/weapon_machinegun_0.png";
 	static final String LASERGUN_PATH = "HeavenGuard/res/images/misc/lasergun_0.png";
 	static final String SHIELDGUN_PATH = "HeavenGuard/res/images/misc/shieldgunammoready.png";
+	
+	static final String CANNONBULLET_PATH = "HeavenGuard/res/images/cannongun/cannonbullet.png";
+	static final String MGBULLET_PATH = "HeavenGuard/res/images/machinegun/machinegun.png";
+	static final String LASERBULLET_PATH = "HeavenGuard/res/images/LaserGun/lasergunammo.png";
+	static final String SHIELDBULLET_PATH = "HeavenGuard/res/images/shieldgun/shieldgunammo.png";
 
 	private static String weaponPath, bulletPath;
 
 	private BufferedImage weaponImage = null;
 	private BufferedImage bulletImage = null;
-
-	private static BaseWeapon baseWeapon = null;
 	
 	public BaseWeapon(WeaponBuilder builder) {
 		
@@ -39,60 +41,9 @@ public class BaseWeapon extends ImageIcon {
 		this.reloadTime = builder.reloadTime;
 		this.fireSpeed = builder.fireSpeed;
 		this.weaponLevel = builder.weaponLevel;
-		this.bulletPath = builder.bulletPath;
 		this.weaponPath = builder.weaponPath;
+		this.bulletPath = builder.bulletPath;
 		
-	}
-
-
-	public BaseWeapon createBaseWeapon(String tag) {
-
-		if(tag.equals("cannon")) {
-
-			weaponPath = CANNON_PATH;
-			bulletPath = "HeavenGuard/res/images/cannongun/cannonbullet.png";
-
-			baseWeapon = new CannonWeapon(100*weapon1lvl, weapon1lvl * 3);
-
-		}
-
-		else if(tag.equals("mg")) {
-
-			weaponPath = MACHINEGUN_PATH;
-			baseWeapon = new MachineGun(weapon2lvl + 1);
-
-		}
-
-		else if(tag.equals("lasergun")) {
-
-			weaponPath = LASERGUN_PATH;
-
-
-			baseWeapon = new LaserGun(30 - (weapon3lvl * 2), (weapon3lvl*10));
-
-		}
-		else if(tag.equals("shieldgun")) {
-
-			weaponPath = SHIELDGUN_PATH;
-
-			baseWeapon = new ShieldGun(60 - (weapon4lvl * 2), (weapon4lvl * 20));
-
-		}
-
-		try {
-
-			weaponImage = ImageIO.read(new File(weaponPath));
-			bulletImage = ImageIO.read(new File(bulletPath));
-
-		} catch(IOException e) {
-
-			System.out.println("Weapon or bullet could not be found");
-
-		}
-
-		baseWeapon.setImage(weaponImage);
-		return baseWeapon;
-
 	}
 
 	public double aimAngle(double mouseX, double mouseY, double weaponX, double weaponY ) {
@@ -113,7 +64,7 @@ public class BaseWeapon extends ImageIcon {
 
 	public void fire(double aimAngle) {
 
-
+		
 
 	}
 
@@ -130,8 +81,6 @@ public class BaseWeapon extends ImageIcon {
 		return weaponPath;
 
 	}
-
-	public static void setWeapon(BaseWeapon weapon) { baseWeapon = weapon; }
 
 }
 
